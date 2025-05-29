76b5ec76aa862b1608a3c84635f5958177bdd0ca
Nguyen Hoc Hoang <bluax1604@gmail.com>
2025-05-30 06:48:04 +0700

    Delete date-time-checker directory

--- date-time-checker/tests/setup_verification_test.js
+++
@@ -1,7 +0,0 @@
-Feature('Quick Setup Verification');
-
-Scenario('Verify CodeceptJS setup is working', ({ I }) => {
-    I.amOnPage('/');
-    I.see('📅 Date & Time Checker');
-    console.log('✅ CodeceptJS setup is working correctly!');
-});
